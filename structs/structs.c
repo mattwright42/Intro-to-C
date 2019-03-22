960d9269bcbb4d4c53a39645034e7e4c4e48a940
Matthew Wright <matthewbwright@gmail.com>
2019-03-22 13:23:47 -0700

    structs is passing tests

--- structs/structs.c
+++ structs/structs.c
@@ -7,9 +7,13 @@
     Person type. Don't forget to specify the type of each field. A 
     Person should have the fields `name`, `age`, `height`, and `weight`.
 */
-typedef struct Person {
-
-} Person;
+struct Person
+{
+    char *name;
+    int age;
+    int height;
+    int weight;
+};
 
 /*
     Creates an instance of the Person struct that receives all the relevant
@@ -19,24 +23,32 @@ typedef struct Person {
     When setting the `name` field, use your previously-built string_dup
     function, which handles the allocation of memory for the new string.
 */
-Person *createPerson(char *name, int age, int height, int weight)
+struct Person *createPerson(char *name, int age, int height, int weight)
 {
+    struct Person *who = malloc(sizeof(struct Person));
 
+    who->name = string_dup(name);
+    who->age = age;
+    who->height = height;
+    who->weight = weight;
+
+    return who;
 }
 
 /*
     Given a pointer to a Person struct, frees up the memory that holds the
     Person's name as well as the memory that holds the Person instance.
 */
-void destroyPerson(Person *who)
+void destroyPerson(struct Person *who)
 {
-
+    free(who->name);
+    free(who);
 }
 
 #ifndef TESTING
 int main(void)
 {
-    Person *tony = createPerson("Tony Stark", 32, 64, 140);
+    struct Person *tony = createPerson("Tony Stark", 32, 64, 140);
 
     printf("  Name: %s\n", tony->name);
     printf("   Age: %d\n", tony->age);
